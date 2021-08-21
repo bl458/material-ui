e9a3401bebdd97d8b02ccb3de4b8989bad24d86e
Michael Lim <bl458@cornell.edu>
2021-08-21 13:39:06 -0400

    CHORE setup material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
-      <p>Hi</p>
-    </div>
-  );
-}
+import React from "react";
+
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
